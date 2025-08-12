886c5af87b17233bb4832a4798d2aed219da47d0
Kiran <kirann.bobby@gmail.com>
2025-08-12 14:54:56 +0530

    Terraform aws eks

---
+++ 80-eks/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z02622157IVZUWOPNGMV"
+}
+
+variable "zone_name" {
+    default = "daws84skiran.store"
+}
